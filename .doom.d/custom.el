720f6d019649b897169a78389a1b5327871cdff1
zachery <momochi000@gmail.com>
2023-03-27 11:18:44 -0700

    search all org files for agenda

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("/home/momochi/Dropbox/my_stuff/org/bookmarks.org" "/home/momochi/Dropbox/my_stuff/org/cheatsheet.org" "/home/momochi/Dropbox/my_stuff/org/finances.org" "/home/momochi/Dropbox/my_stuff/org/house_hunting.org" "/home/momochi/Dropbox/my_stuff/org/inbox.org" "/home/momochi/Dropbox/my_stuff/org/journal.org" "/home/momochi/Dropbox/my_stuff/org/mbp_setup.org" "/home/momochi/Dropbox/my_stuff/org/mommy_projects.org" "/home/momochi/Dropbox/my_stuff/org/music.org" "/home/momochi/Dropbox/my_stuff/org/projects.org" "/home/momochi/Dropbox/my_stuff/org/recurring_payments.org" "/home/momochi/Dropbox/my_stuff/org/study.org" "/home/momochi/Dropbox/my_stuff/org/thoughts.org" "/home/momochi/Dropbox/my_stuff/org/vacation.org" "/home/momochi/Dropbox/my_stuff/org/wisdom.org")))
+    '(org-agenda-files (directory-files-recursively "~/Dropbox/my_stuff/org/" "\\.org$"))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
